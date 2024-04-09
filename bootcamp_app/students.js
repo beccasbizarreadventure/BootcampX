d678fe1f9ec503f038095e4dca309a86472d1d7b
Rebecca <rebecca90469@gmail.com>
2024-04-08 22:22:36 -0400

    Practice

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+
+pool
+  .query(
+    `SELECT students.id as id, students.name as name, cohorts.name as cohort
+     FROM students
+     JOIN cohorts ON cohorts.id = students.cohort_id
+     WHERE cohorts.name LIKE '%${process.argv[2]}%'
+     LIMIT ${process.argv[3] || 5};`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
